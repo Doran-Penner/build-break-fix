e70b6c914af05c1d349b758b0f9f973402180951
Doran Penner <doranaviv@gmail.com>
2025-04-15 14:42:04 -0700

    crypto: save salt

--- good_example.py
+++ good_example.py
@@ -1,5 +1,5 @@
 # <https://cryptography.io/en/latest/fernet/#using-passwords-with-fernet>
-from os import urandom
+from os import urandom, path
 from base64 import urlsafe_b64encode
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
@@ -8,7 +8,20 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 password = "input token".encode()  # or some way to get the bytes
 # also assuming password is alphanumeric etc, parsing is a whole other issue
 
-salt = urandom(16)  # FIXME need to store the salt somehow
+# store salt file next to log file
+log_file = "EXAMPLES.md"  # or whatever we get from input
+salt_file = path.dirname(log_file)
+if salt_file == "":
+    salt_file = "."
+salt_file += "/salt"
+
+# if it's already stored, read it; otherwise make new salt
+if path.exists(salt_file):
+    with open(salt_file, "rb") as f:
+        salt = f.read()
+else:
+    salt = urandom(16)
+
 kdf = PBKDF2HMAC(
     algorithm=hashes.SHA256(),
     length=32,
@@ -19,7 +32,7 @@ key = urlsafe_b64encode(kdf.derive(password))
 
 black_box = Fernet(key)
 
-with open("EXAMPLES.md", "rb") as f:
+with open(log_file, "rb") as f:
     plaintext = f.read()
 
 ciphertext = black_box.encrypt(plaintext)
@@ -27,3 +40,7 @@ ciphertext = black_box.encrypt(plaintext)
 plaintext_new = black_box.decrypt(ciphertext)
 
 print(plaintext_new)
+
+# save salt to file for later
+with open(salt_file, "wb") as f:
+    f.write(salt)
