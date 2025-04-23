08ac595f85de79db25519a17234ebf1344918770
Doran Penner <doranaviv@gmail.com>
2025-04-22 20:45:00 -0700

    crypto: save everything together, clean up code

--- src/loadsave.py
+++ src/loadsave.py
@@ -18,7 +18,14 @@ save_all(log, password, log_file)
 
 And then you're done! You shouldn't need to use
 anything else in loadsave, just those two functions.
+
+The format of the saved logfile is ciphertext || salt || nonce,
+where salt is set once when the log is created and the
+nonce is re-randomized every time we modify the file.
 """
+
+SALT_LENGTH = 16
+NONCE_LENGTH = 12
 
 
 class Event:
@@ -54,42 +61,6 @@ class Event:
     __repr__ = __str__
 
 
-def get_adj_path(name: str, log_file: str) -> str:
-    adj_file = os.path.dirname(log_file)
-    if adj_file == "":
-        adj_file = "."
-    adj_file += "/" + name
-    return adj_file
-
-
-def get_salt(log_file: str) -> bytes:
-    salt_file = get_adj_path("salt", log_file)
-    if os.path.exists(salt_file):
-        with open(salt_file, "rb") as f:
-            # NOTE: we explicitly do not use pickle on the salt file,
-            # that would be insecure and we don't need it
-            salt = f.read()
-    else:
-        salt = os.urandom(16)
-    return salt
-
-
-def get_nonce(log_file: str, decrypting: bool) -> bytes:
-    file_path = get_adj_path("nonce", log_file)
-    if os.path.exists(file_path) and decrypting:
-        with open(file_path, "rb") as f:
-            nonce = f.read()
-    else:
-        nonce = os.urandom(12)
-    return nonce
-
-
-def save_adj(name: str, val: bytes, log_file: str):
-    file_path = get_adj_path(name, log_file)
-    with open(file_path, "wb") as f:
-        f.write(val)
-
-
 # <https://cryptography.io/en/latest/fernet/#using-passwords-with-fernet>
 def get_black_box(password: str, salt: bytes) -> ChaCha20Poly1305:
     kdf = PBKDF2HMAC(
@@ -102,32 +73,35 @@ def get_black_box(password: str, salt: bytes) -> ChaCha20Poly1305:
     return ChaCha20Poly1305(key)
 
 
-def load_all(password: str, log_file: str) -> list[Event]:
+def load_all(password: str, log_file: str) -> tuple[list[Event], bytes]:
     if os.path.exists(log_file):
-        salt = get_salt(log_file)
-        nonce = get_nonce(log_file, True)
-        black_box = get_black_box(password, salt)
         with open(log_file, "rb") as f:
             ciphertext = f.read()
-        return pickle.loads(black_box.decrypt(nonce, ciphertext, None))
+        salt = ciphertext[-(SALT_LENGTH + NONCE_LENGTH) : -(NONCE_LENGTH)]
+        nonce = ciphertext[-(NONCE_LENGTH):]
+        ciphertext = ciphertext[: -(SALT_LENGTH + NONCE_LENGTH)]
+        black_box = get_black_box(password, salt)
+        log = pickle.loads(black_box.decrypt(nonce, ciphertext, None))
     else:
-        return []
+        log = []
+        salt = os.urandom(SALT_LENGTH)
+    # If we don't keep salt in memory, attacker can overwrite salt between
+    # load & save and then they get to decide our salt. Not sure what the
+    # exact attack is there, but it feels like a bad idea!
+    return log, salt
 
 
-def save_all(log: list[Event], password: str, log_file: str):
-    salt = get_salt(log_file)
-    nonce = get_nonce(log_file, False)
+def save_all(log: list[Event], salt: bytes, password: str, log_file: str):
+    nonce = os.urandom(12)
     black_box = get_black_box(password, salt)
-    # ideally we save the nonce and salt as added & auth'd data via chachapoly
-    # but I can't figure out how to read that so we'll do the normal thing
     enc_log = black_box.encrypt(nonce, pickle.dumps(log), None)
+    enc_log += salt + nonce
     with open(log_file, "wb") as f:
         f.write(enc_log)
-    save_adj("salt", salt, log_file)
-    save_adj("nonce", nonce, log_file)
 
 
-# log: list[Event] = load_all("sekwet pasword :3", "./logfile")
+# log: list[Event]
+# log, salt = load_all("sekwet pasword :3", "./logfile")
 # log.append(Event(2, True, "Jane", True, 1))
 # print(log)
-# save_all(log, "sekwet pasword :3", "./logfile")
+# save_all(log, salt, "sekwet pasword :3", "./logfile")
